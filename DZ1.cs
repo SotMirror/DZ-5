7126541013cecb3b139add47917d93797571352d
SotMirror <110252714+SotMirror@users.noreply.github.com>
2022-09-04 17:30:25 +0300

    Add files via upload

---
+++ DZ1.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int j = 0; j < numbers.Length; j++)
+if (numbers[j] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,999);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.WriteLine();
+}
